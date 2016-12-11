62adaf2816f07dc84330d50cbd93d919deb777e4
Jesus Oviedo Riquelme <j92riquelme@gmail.com>
2016-12-11 14:05:10 -0300

    Update

--- Operation.java
+++
@@ -1,8 +0,0 @@
-package com.jesus.java.interfazfunc;
-
-@FunctionalInterface
-public interface Operacion {
-//	intefaz funcional, contiene un unico metodo
-//	double calcular2(double n1, double n2);
-	double calcular(double n1, double n2);
-}
